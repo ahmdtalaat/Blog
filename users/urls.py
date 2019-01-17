2d7e60a31277c99a8e555b7dfc44bdc147a9e571
Talaet <talaat995@gmail.com>
2019-01-17 22:06:33 +0200

    Add Login and logout to the blog with profile page.

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import signup
+from django.contrib.auth import views as auth_views
+from .views import signup, profile
+
 app_name = "users"
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="registration/logout.html"), name='logout'),
+    path('profile/', profile, name='profile')
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import SignUpForm
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 def signup(request):
@@ -10,9 +12,14 @@ def signup(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect("blog:home")
+            messages.success(request, f'Account created for {username}, Now you can login')
+            return redirect("users:login")
 
     else:
         form = SignUpForm()
-        return render(request, 'users/signup.html', {'form': form})
+    return render(request, 'users/signup.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html',)
